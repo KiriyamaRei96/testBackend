c5b92ff9cc723294286d60f099be06925f338c8d
HoangFE <lamduchoa96@gmail.com>
2023-05-31 16:37:12 +0700

    update

--- dist/index.js
+++ dist/index.js
@@ -7,8 +7,11 @@ const express_1 = __importDefault(require("express"));
 const morgan_1 = __importDefault(require("morgan"));
 const path_1 = __importDefault(require("path"));
 const express_handlebars_1 = require("express-handlebars");
+const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
 const port = 3000;
+// body parser
+app.use(body_parser_1.default.urlencoded({ extended: true }));
 // static file
 app.use(express_1.default.static(path_1.default.join(__dirname, "../src/public")));
 // request logger
@@ -20,8 +23,21 @@ app.set("views", path_1.default.join(__dirname, "../src/resources/views"));
 app.get("/", (req, res) => {
     res.render("home", { title: "Home" });
 });
+app.get("/search", (req, res) => {
+    res.render("search");
+});
+app.post("/search", (req, res) => {
+    console.log(req);
+    res.send("search");
+});
 app.get("/news", (req, res) => {
     res.render("news", { title: "News" });
+});
+//If no route is matched by now, it must be a 404
+app.use(function (req, res, next) {
+    res.status(404);
+    res.send("Page not found");
+    next();
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,11 @@ import express from "express";
 import morgan from "morgan";
 import path from "path";
 import { engine } from "express-handlebars";
+import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
+// body parser
+app.use(bodyParser.urlencoded({ extended: true }));
 // static file
 app.use(express.static(path.join(__dirname, "../src/public")));
 
@@ -17,8 +20,22 @@ app.set("views", path.join(__dirname, "../src/resources/views"));
 app.get("/", (req, res) => {
   res.render("home", { title: "Home" });
 });
+app.get("/search", (req, res) => {
+  res.render("search");
+});
+app.post("/search", (req, res) => {
+  console.log(req);
+  res.send("search");
+});
 app.get("/news", (req, res) => {
   res.render("news", { title: "News" });
+});
+//If no route is matched by now, it must be a 404
+
+app.use(function (req, res, next) {
+  res.status(404);
+  res.send("Page not found");
+  next();
 });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
