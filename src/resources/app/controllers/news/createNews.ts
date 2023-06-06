bca34a4e7c2f19f254f63428e918010b9c0dab85
rei <lamduchoa96@gmail.com>
2023-06-07 00:23:22 +0700

    change divce

--- src/resources/app/controllers/news/createNews.ts
+++ src/resources/app/controllers/news/createNews.ts
@@ -1,15 +1,18 @@
 import { NextFunction, Request, Response } from 'express';
 import News from '../../models/news';
+import { ResponseType } from '../../../utils/type';
 
 async function createNews(req: Request, res: Response, next: NextFunction) {
   try {
-    const data = req.body;
-    if (data.relate) {
-      data.relate = data.relate.split(',');
+    const resData = req.body;
+    if (resData.relate) {
+      resData.relate = resData.relate.split(',');
     }
-    const newData = await News.create(data);
-    const resData = await News.findById(newData.id).populate('relate');
-    res.status(201).json(resData);
+
+    const newData = await News.create(resData);
+    const data = await News.findById(newData.id).populate('relate');
+    const resItem: ResponseType = { data, status: 1 };
+    res.status(201).json(resItem);
   } catch (err: any) {
     const errors: any = {};
     Object.keys(err.errors).forEach(
---
+++ src/resources/utils/type.ts
@@ -0,0 +1,5 @@
+export interface ResponseType {
+  data?: any;
+  message?: string;
+  status: 1;
+}
